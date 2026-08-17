925870f3b396c7091753ceff31793da76691e07e
sugal <sugaldarlami@gmail.com>
2026-08-17 10:23:22 +0545

    Day 1

---
+++ Js/index.js
@@ -0,0 +1,58 @@
+//consoles
+console.log("Sugal");
+console.warn("warning");
+console.error("Error");
+const name = "Shyam";
+
+
+//arithmetic
+let a = 6, b = 2;
+let sum = a + b;
+let subtraction = a - b;
+let multiply = a * b;
+let Division = a / b;
+let remainder = a % b;
+let exponent = a ** b;
+
+console.log("Sum:", sum);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiply);
+console.log("Division:", Division);
+console.log("Remainder:", remainder);
+console.log("Exponent:", exponent);
+
+//comparison
+let c = 10, d = 5;
+
+console.log(c == d);
+console.log(c === d);
+
+console.log(c != d);
+console.log(c !== d);
+
+console.log(c > d);
+console.log(c < d);
+
+console.log(c >= d);
+console.log(c <= d);
+
+
+
+//ternary operator
+let age = 20;
+let result = age >= 18 ? "Adult" : "Minor";
+console.log(result);
+
+//Convert number to string
+let g = 123;
+let strin = String(g);
+console.log(typeof strin);
+
+//Convert string to number
+let x = "123";
+let num = Number(x);
+console.log(typeof num);
+
+
+
+
